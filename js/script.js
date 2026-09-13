d20ebffa0b491b789b7612c28d80e83165003003
emyu-collab <hafuzafashakhilfi@gmail.com>
2026-09-13 14:12:42 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    alert("Pendaftaran berhasil!");
+});
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let nama = document.querySelector("#nama").value;
+    let email = document.querySelector("#email").value;
+    let kelas = document.querySelector("#kelas").value;
+    let jurusan = document.querySelector("#jurusan").value;
+    let kegiatan = document.querySelector("#kegiatan").value;
+    let setuju = document.querySelector("#setuju").checked;
+
+    if (nama === "" || email === "" || kelas === "" || jurusan === "" || kegiatan === "") {
+        alert("⚠️ Mohon lengkapi semua data!");
+    } 
+    else if (!setuju) {
+        alert("⚠️ Kamu harus menyetujui Syarat & Ketentuan!");
+    } 
+    else {
+        alert("✅ Pendaftaran berhasil!");
+    }
+});
